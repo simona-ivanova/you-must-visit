7425afca5980989ba0a01d2618118c456bc900cb
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-07-11 22:48:16 +0300

    Create angular app and connect with firebase

---
+++ you-must-visit/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ you-must-visit/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase : {
+        apiKey: "AIzaSyDjUIymsxEh5kNAbxsQ7NMmNEjnFD8KB50",
+        authDomain: "you-must-visit.firebaseapp.com",
+        projectId: "you-must-visit",
+        storageBucket: "you-must-visit.appspot.com",
+        messagingSenderId: "853760313875",
+        appId: "1:853760313875:web:8ff31393510506eeb44de6",
+        measurementId: "G-SVW5GXWWKW"
+    }
+};

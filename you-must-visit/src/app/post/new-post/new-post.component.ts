3dd52553badee8209da068800bbe8a6eefb4a551
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-08-07 11:45:21 +0300

    Add new post functionality

--- you-must-visit/src/app/post/new-post/new-post.component.ts
+++ you-must-visit/src/app/post/new-post/new-post.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-new-post',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  newPostSubmitHandler(form: NgForm): void {
+
+    if (form.invalid) {
+      return;
+    }
+
+    this.apiService.addData(form.value)
+      .then(() => {
+        console.log('Data Saved Successfuly');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 }

1628ca5b1ae1de067479e2e16c75d373da4a4895
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-08-17 12:15:44 +0300

    Add greeting to the navigation

--- you-must-visit/src/app/core/header/header.component.ts
+++ you-must-visit/src/app/core/header/header.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth, } from '@angular/fire/compat/auth';
+import { AuthService } from 'src/app/user/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+
+export class HeaderComponent implements OnInit {
+  username!: string;
+  isAuthenticated: boolean = true;
+
+  constructor(private authService: AuthService,
+    public fireauth: AngularFireAuth
+  ) { }
+
+  ngOnInit(): void {
+    let userEmail:any;
+
+    this.fireauth.onAuthStateChanged((user) => {
+      if (user) {
+        userEmail = user.email
+      } else {
+        return undefined
+      }
+      let initialUsername = userEmail.split("@")[0];
+      this.username = initialUsername.charAt(0).toUpperCase() + initialUsername.slice(1);
+    })
+  };
+
+  isLoggedIn() {
+    this.isAuthenticated = this.authService.isLoggedIn();
+    return this.isAuthenticated
+  }
 
 }

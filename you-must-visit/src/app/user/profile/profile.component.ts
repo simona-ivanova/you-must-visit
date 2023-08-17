1228d16bfd3a6ea94a0858852513787a02bfb2b0
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-08-17 22:43:49 +0300

    Edit profile.css

--- you-must-visit/src/app/user/profile/profile.component.ts
+++ you-must-visit/src/app/user/profile/profile.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
-  constructor(private auth: AuthService, private router: Router) { }
+export class ProfileComponent implements OnInit {
+  username!: string;
+  userEmail: any;
+
+  constructor(private auth: AuthService, private router: Router, public fireauth: AngularFireAuth) { }
+
+  ngOnInit(): void {
+    this.fireauth.onAuthStateChanged((user) => {
+      if (user) {
+        this.userEmail = user.email
+      } else {
+        return undefined
+      }
+      let initialUsername = this.userEmail.split("@")[0];
+      this.username = initialUsername.charAt(0).toUpperCase() + initialUsername.slice(1);
+    })
+  }
 
   logout() {
     this.auth.logout();
